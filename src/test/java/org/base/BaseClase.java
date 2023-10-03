b52e7933fe474da928345d3fc5be370fc07b30e6
seraraja <seraraja555@gmail.com>
2023-10-03 09:40:31 +0530

    project structure

---
+++ src/test/java/org/base/BaseClase.java
@@ -0,0 +1,23 @@
+package org.base;
+
+public class BaseClase {
+
+	public static int empId() {
+		int a=10;
+		System.out.println("sera");
+		System.out.println("sera");
+		System.out.println("sera");
+		System.out.println("sera");
+		System.out.println("sera");
+		return a;
+
+	}
+	
+	
+	public static void main(String[] args) {
+		int empId = empId();
+		System.out.println(empId);
+		
+	}
+	
+}
---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,36 @@
+package org.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.facebook.com/");
+		
+		WebElement txtUsername = driver.findElement(By.id("email"));
+		txtUsername.sendKeys("sera");
+		
+		WebElement txtPassword = driver.findElement(By.id("pass"));
+		txtPassword.sendKeys("9898");
+		
+		WebElement btnLogin = driver.findElement(By.name("login"));
+		btnLogin.click();
+		
+		
+		
+		
+	}
+	
+}
